24219f12d384fdfe0727a750886aee86b9340cb3
Claudio Cordero <cordero.guiza@gmail.com>
2024-03-20 10:37:16 +0100

    exercici corregit

---
+++ src/main/java/n1exercici2/main/AnimalsMain.java
@@ -0,0 +1,12 @@
+package n1exercici2.main;
+
+import n1exercici2.handlers.Animals;
+
+public class AnimalsMain {
+
+	public static void main(String[] args) {
+		
+		Animals demo = new Animals();
+		demo.filtrarLlista();
+	}
+}
